9eeca08b3822a9742400fecf9e7fe59fd65ef874
Rebecca Brietzke <rnbrietzke@outlook.com>
2023-01-27 10:36:33 -0600

    add companies route and input for searching jobs an companies in SearchPage component

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
+import Home from './components/Home/Home';
+import SearchPage from './components/SearchPage/SearchPage';
 const JoblyRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/signup" element={<Home />} />
+      <Route path="/companies" element={<SearchPage />} />
     </Routes>
   );
 };
--- frontend/src/components/SearchPage/SearchPage.js
+++ frontend/src/components/SearchPage/SearchPage.js
@@ -1,1 +1,33 @@
 /* Reneders search input and coresponding jobs or companies */
+import React, { useState } from 'react';
+
+const SearchPage = () => {
+  const types = ['Companies', 'Jobs'];
+  const [searchTerm, setSearchTerm] = useState('');
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  console.log(searchTerm);
+  return (
+    <div>
+      <form>
+        <label htmlFor="search"></label>
+        <input
+          id="search"
+          type="text"
+          name="search"
+          value={searchTerm}
+          placeholder={`Search for ${types[0]}`}
+          className="form-input"
+          onChange={handleChange}
+        />
+        <button>Search</button>
+      </form>
+      <main className="SearcPage-details">
+        {/* map over jobs or companies */}
+      </main>
+    </div>
+  );
+};
+
+export default SearchPage;

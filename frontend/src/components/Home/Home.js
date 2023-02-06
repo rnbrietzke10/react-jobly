1a6a0fe8e24db2251ecbc0a3043c849892c22d91
Rebecca Brietzke <rnbrietzke@outlook.com>
2023-02-06 14:14:33 -0600

    work on styling for hompage when no user is logged in

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Home.css';
 const Home = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -6,20 +7,22 @@ const Home = () => {
   const signupUser = () => {};
   const loginUser = () => {};
   return (
-    <main>
-      <div>
-        <h2>Jobly</h2>
-        <p>Find your next career!</p>
+    <main className="Home__container">
+      <h2 className="Home__title">Jobly</h2>
+      <p>Find your next career!</p>
 
-        {isLoggedIn ? (
-          <h3>Welcome Back {firstName}</h3>
-        ) : (
-          <div>
-            <button onClick={signupUser}>Sign Up</button>
-            <button onClick={loginUser}>Login</button>
-          </div>
-        )}
-      </div>
+      {isLoggedIn ? (
+        <h3>Welcome Back {firstName}</h3>
+      ) : (
+        <div className="Home__btn-container">
+          <Link to="/signup" className="btn Home__btn" onClick={signupUser}>
+            Sign Up
+          </Link>
+          <Link to="/login" className="btn Home__btn" onClick={loginUser}>
+            Login
+          </Link>
+        </div>
+      )}
     </main>
   );
 };

251006e83bd968ce552be62bd04bc15d8d4ceedb
Rebecca Brietzke <rnbrietzke@outlook.com>
2023-02-06 08:02:36 -0600

    work on comapny component

--- frontend/src/components/Company/Company.js
+++ frontend/src/components/Company/Company.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const Company = () => {
+const Company = ({ company }) => {
   return (
     <div>
-      <div></div>
+      <div>
+        <h3>{company.name}</h3>
+        <h5>Number of Employees: {company.numEmployees}</h5>
+        <p>{company.description}</p>
+      </div>
+      <div>
+        <img src={company.logoUrl} alt={`${company.name}'s logo`} />
+      </div>
     </div>
   );
 };
